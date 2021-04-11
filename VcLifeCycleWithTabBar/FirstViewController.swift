d8292b091f1275626959ce3be5633460624aa75c
Shayla Parvin <shaylahstu2015@gmail.com>
2021-04-11 17:11:15 +0600

    Initial Commit

---
+++ VcLifeCycleWithTabBar/FirstViewController.swift
@@ -0,0 +1,41 @@
+//
+//  FirstViewController.swift
+//  VcLifeCycleWithTabBar
+//
+//  Created by Shaikat on 11/4/21.
+//  Copyright © 2021 Shayla.18. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        print("First View Controller - View Did Load")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("First View Controller - View Will Appear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("First View Controller - View Did Appear")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("First View Controller - View Will Disappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("First View Controller - View Did Disappear")
+        
+    }
+    
+}
